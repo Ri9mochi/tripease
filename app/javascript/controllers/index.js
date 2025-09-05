69d3e4033fcadf23625743c9d80a44bcae6db44c
Ri9mochi <rikujobs211111@gmail.com>
2025-09-05 18:59:28 +0900

    Reapply "本番環境に合わせたjavascriptの修正"
    This reverts commit 99bcab3abf1c4a5a85205a0beb3e7bc6abe4408f.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,10 @@
 // app/javascript/controllers/index.js
 
-import { application } from "controllers/application"
+import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-eagerLoadControllersFrom("controllers", application)
+// eagerLoadControllersFrom("controllers", application)をコメントアウトまたは削除し、
+// Stimulusコントローラーをまとめて読み込むように修正します。
 
-import DestinationFilterController from "./destination_filter_controller.js"
-application.register("destination-filter", DestinationFilterController)
+// Stimulusコントローラーを自動的に読み込む
+eagerLoadControllersFrom("controllers", application)
