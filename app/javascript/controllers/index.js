6a81de17dcaae125b026be14da4ae9f232791fbf
Ri9mochi <rikujobs211111@gmail.com>
2025-09-05 17:46:08 +0900

    本番環境に合わせたjavascriptの修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,10 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+// これで app/javascript/controllers 配下の *_controller.js を自動登録
 eagerLoadControllersFrom("controllers", application)
 
-import { Application } from "@hotwired/stimulus"
+// 手動登録が必要な場合は同じ application を使う
 import DestinationFilterController from "./destination_filter_controller"
-
-window.Stimulus = Application.start()
-Stimulus.register("destination-filter", DestinationFilterController)
+application.register("destination-filter", DestinationFilterController)
