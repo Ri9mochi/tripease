6c99ff13de808a082972a55938f4d27ffc209b81
Ri9mochi <rikujobs211111@gmail.com>
2025-09-05 17:21:16 +0900

    Revert "Reapply "本番環境デプロイ修正""
    This reverts commit 4170a8ebe1eccc566f3b0f708ac303edc6c2685c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'devise/sessions#new'
-  
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   authenticated :user do
     root to: 'travel_plans#index', as: :authenticated_root
   end
