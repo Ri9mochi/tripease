99bcab3abf1c4a5a85205a0beb3e7bc6abe4408f
Ri9mochi <rikujobs211111@gmail.com>
2025-09-05 18:15:54 +0900

    Revert "本番環境に合わせたjavascriptの修正"
    This reverts commit 6a81de17dcaae125b026be14da4ae9f232791fbf.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,9 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
+// app/javascript/controllers/index.js
+
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-// これで app/javascript/controllers 配下の *_controller.js を自動登録
 eagerLoadControllersFrom("controllers", application)
 
-// 手動登録が必要な場合は同じ application を使う
-import DestinationFilterController from "./destination_filter_controller"
+import DestinationFilterController from "./destination_filter_controller.js"
 application.register("destination-filter", DestinationFilterController)
