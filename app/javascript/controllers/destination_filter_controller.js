058159e9c87ca07966c4da2b60e87d0873acd40f
Ri9mochi <rikujobs211111@gmail.com>
2025-09-07 07:52:38 +0900

    Reapply "Reapply "Reapply "Reapply "本番環境に合わせたjavascriptの修正""""
    This reverts commit ce3d8a4d1215bbea8ec945e9dbc1c37527ae3fb1.

--- app/javascript/controllers/destination_filter_controller.js
+++ app/javascript/controllers/destination_filter_controller.js
@@ -4,17 +4,14 @@ export default class extends Controller {
   static targets = ["groupSelect", "destination"]
 
   connect() {
-    // ページ読み込み時に、全てのチェックボックスを非表示にする
-    this.destinationTargets.forEach(el => {
-      el.style.display = "none";
-    });
+    this.update() // ページ読み込み時も非表示にする
   }
 
   update() {
     const selectedGroupId = this.groupSelectTarget.value
 
     this.destinationTargets.forEach(el => {
-      // 選択されたグループIDが空の場合は全て非表示
+      // 選択が空の場合は全て非表示
       if (selectedGroupId && el.dataset.prefectureGroup === selectedGroupId) {
         el.style.display = "block"
       } else {
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,4 @@
-// app/javascript/controllers/index.js
-
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-// eagerLoadControllersFrom("controllers", application)をコメントアウトまたは削除し、
-// Stimulusコントローラーをまとめて読み込むように修正します。
-
-// Stimulusコントローラーを自動的に読み込む
 eagerLoadControllersFrom("controllers", application)
