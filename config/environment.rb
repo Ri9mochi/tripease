ce3d8a4d1215bbea8ec945e9dbc1c37527ae3fb1
Ri9mochi <rikujobs211111@gmail.com>
2025-09-07 07:46:23 +0900

    Revert "Reapply "Reapply "Reapply "本番環境に合わせたjavascriptの修正""""
    This reverts commit c1d66ff7b998d7d623b6bff37665f055810d1fb6.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# public/ 配下の静的ファイル配信を ENV で制御
+config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+# Sprockets の推奨設定（Importmap + Sprockets の標準）
+config.assets.compile = false
