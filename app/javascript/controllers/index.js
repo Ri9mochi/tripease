39c3abbe5c170be94eee3f7af625268674a140b0
Ri9mochi <rikujobs211111@gmail.com>
2025-09-08 10:09:02 +0900

    javascript本番環境頭皮互換調整v03

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { Application } from "@hotwired/stimulus"
+import DestinationFilterController from "./destination_filter_controller"
 
-eagerLoadControllersFrom("controllers", application)
+window.Stimulus = Application.start()
+Stimulus.register("destination-filter", DestinationFilterController)
