c5268361e0fd12351c144d319753b432268a07fa
Ri9mochi <rikujobs211111@gmail.com>
2025-09-08 11:57:52 +0900

    javascript本番環境との非互換調整v05
    Stimulus を完全に外して純粋 JavaScriptで動くよう修正

--- app/javascript/controllers/index.js
+++
@@ -1,5 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-import DestinationFilterController from "./destination_filter_controller"
-
-window.Stimulus = Application.start()
-Stimulus.register("destination-filter", DestinationFilterController)
