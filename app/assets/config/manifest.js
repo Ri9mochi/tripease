53341f747e51741600a145a9e099b7ceee304b3c
Ri9mochi <rikujobs211111@gmail.com>
2025-09-10 11:46:30 +0900

    ビューの修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link travel_plans/preview.css
+//= link travel_plans/show.css
